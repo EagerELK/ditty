9dde92831c2f3762a81286b27ab5bae5d45431d0
Jurgens du Toit <jrgns@jadeit.co.za>
2017-10-04 16:54:38 +0200

    chore: Move Sequel plugins and extensions back to db.rb

--- lib/ditty/db.rb
+++ lib/ditty/db.rb
@@ -10,3 +10,9 @@ DB = Sequel.connect(ENV['RACK_ENV'] == 'production' ? ENV.delete('DATABASE_URL')
 log_level = (ENV['SEQUEL_LOGGING_LEVEL'] || :debug).to_sym
 DB.sql_log_level = log_level
 DB.loggers << Ditty::Services::Logger.instance
+
+Sequel::Model.plugin :validation_helpers
+Sequel::Model.plugin :update_or_create
+Sequel::Model.plugin :timestamps, update_on_create: true
+
+DB.extension(:pagination)
