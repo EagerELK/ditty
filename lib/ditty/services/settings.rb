385b1d25b114103c49c8f7c5ac71733ff87379f7
Jurgens du Toit <jrgns@jadeit.co.za>
2018-05-19 22:07:40 +0200

    chore: Settings spec

--- lib/ditty/services/settings.rb
+++ lib/ditty/services/settings.rb
@@ -2,39 +2,32 @@
 
 require 'yaml'
 require 'erb'
+require 'active_support/core_ext/hash/deep_merge'
+require 'active_support/core_ext/hash/keys'
 
 module Ditty
   module Services
     module Settings
-      CONFIG = './config/settings.yml'.freeze
+      CONFIG_FOLDER = './config'.freeze
 
       class << self
         def [](key)
           values[key.to_sym]
         end
 
-        def values
-          @values ||= values!
-        end
-
-        def values!
-          @values = begin
-            base = read
-            base.merge!(base[ENV['APP_ENV'].to_sym]) if ENV['APP_ENV']
-            base
+        def values(scope = :settings)
+          @values ||= Hash.new do |h, k|
+            h[k] = read("#{CONFIG_FOLDER}/#{k}.yml") if File.file?("#{CONFIG_FOLDER}/#{k}.yml")
+            h[k] = h[:settings][k] if h.key?(k) == false && h.key?(:settings) && k != :settings
+            h.key?(k) ? h[k] : {}
           end
+          @values[scope]
         end
 
-        private
-
-        def read
-          symbolize_keys(File.exist?(CONFIG) ? YAML.safe_load(ERB.new(File.read(CONFIG)).result) : {})
-        end
-
-        def symbolize_keys(hash)
-          return hash.map { |v| symbolize_keys(v) } if hash.is_a? Array
-          return hash unless hash.is_a? Hash
-          Hash[hash.map { |k, v| [k.to_sym, symbolize_keys(v)] }]
+        def read(filename)
+          base = YAML.safe_load(ERB.new(File.read(filename)).result).deep_symbolize_keys
+          base.deep_merge!(base[ENV['APP_ENV'].to_sym]) unless ENV['APP_ENV'].nil? || base[ENV['APP_ENV'].to_sym].nil?
+          base
         end
       end
     end
