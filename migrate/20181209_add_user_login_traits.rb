d25f05306846711474600a4e6a7e893ba8db20ad
Jurgens du Toit <jrgns@automationexchange.co.za>
2019-06-25 21:22:42 +0200

    fix: Use null in migrations, not nullable

--- migrate/20181209_add_user_login_traits.rb
+++ migrate/20181209_add_user_login_traits.rb
@@ -5,10 +5,10 @@ Sequel.migration do
     create_table :user_login_traits do
       primary_key :id
       foreign_key :user_id, :users
-      String :ip_address, nullable: true
-      String :platform, nullable: true
-      String :device, nullable: true
-      String :browser, nullable: true
+      String :ip_address, null: true
+      String :platform, null: true
+      String :device, null: true
+      String :browser, null: true
       DateTime :created_at
       DateTime :updated_at
     end
