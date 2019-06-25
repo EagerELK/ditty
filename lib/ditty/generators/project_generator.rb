1055f4217c051542d9abc14cd963e5c62384537d
Jurgens du Toit <jrgns@automationexchange.co.za>
2019-06-25 23:09:56 +0200

    chore: Generate top module file

--- lib/ditty/generators/project_generator.rb
+++ lib/ditty/generators/project_generator.rb
@@ -8,47 +8,40 @@ module Ditty
     class ProjectGenerator < Thor::Group
       include Thor::Actions
 
+      attr_reader :namespace, :folder
+
       desc 'Initialize a new Ditty project in the current folder'
+
+      def setup
+        name = File.basename(Dir.getwd)
+        @namespace = name.classify
+        @folder    = namespace.underscore
+      end
 
       def self.source_root
         File.expand_path('../templates', __dir__)
       end
 
       def create_startup_files
-        copy_file 'config.ru', './config.ru'
-        copy_file 'application.rb', './application.rb'
-        copy_file 'Rakefile', './Rakefile'
+        directory 'logs'
+        directory 'pids'
         copy_file '.gitignore', './.gitignore'
         copy_file 'env.example', './.env'
+
+        template 'lib/project.rb.erb', "lib/#{folder}.rb"
+
+        copy_file 'application.rb', './application.rb'
+        copy_file 'config.ru', './config.ru'
+        copy_file 'Rakefile', './Rakefile'
         copy_file 'sidekiq.rb', './config/sidekiq.rb'
         copy_file 'sidekiq.yml', './config/sidekiq.yml'
-        copy_file 'settings.yml', './config/settings.yml'
+
         copy_file 'spec_helper.rb', './specs/spec_helper.rb'
         copy_file '../../../spec/support/api_shared_examples.rb', './specs/support/api_shared_examples.rb'
         copy_file '../../../spec/support/crud_shared_examples.rb', './specs/support/crud_shared_examples.rb'
-        directory 'logs'
-        directory 'pids'
-        # spec/support/api_shared_examples
-        # spec/support/crud_shared_examples
+
+        copy_file 'settings.yml', './config/settings.yml'
       end
-
-      # def create_controller
-      #   filename = File.join("lib/#{folder}/controllers", "#{controller_name.underscore}.rb")
-      #   template '../templates/controller.rb.erb', filename
-      #   # TODO: Insert the route into the component file
-      #   # insert_into_file 'config.ru', "use #{class_name}\n", after: 'run ApplicationController\n'
-      # end
-
-      # def create_policy
-      #   filename = File.join("lib/#{folder}/policies", "#{policy_name.underscore}.rb")
-      #   template '../templates/policy.rb.erb', filename
-      # end
-
-      # def create_views
-      #   return unless options[:views]
-
-      #   directory '../templates/views', views_folder
-      # end
     end
   end
 end
