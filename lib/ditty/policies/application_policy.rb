ea112000f4d12023e5c7f2229600dc07428f7b5f
Jurgens du Toit <jrgns@jadeit.co.za>
2020-03-08 07:36:49 +0200

    Revert "chore: Automatic aliases for permitted_Attributes_for in policies"
    This reverts commit 8a61573383779a8fea86d02c7a3354bd00792b87.

--- lib/ditty/policies/application_policy.rb
+++ lib/ditty/policies/application_policy.rb
@@ -9,26 +9,6 @@ module Ditty
       @record = record
     end
 
-    def permitted_attributes
-      []
-    end
-
-    def method_missing(method_name, *args, &block)
-      return super unless method_name.to_s.start_with? 'permitted_attributes_for_'
-
-      permitted_attributes
-    end
-
-    def respond_to_missing?(method_name, _include_private = false)
-      return super unless method_name.to_s.start_with? 'permitted_attributes_for_'
-
-      true
-    end
-
-    def response_attributes
-      permitted_attributes
-    end
-
     class Scope
       attr_reader :user, :scope
 
