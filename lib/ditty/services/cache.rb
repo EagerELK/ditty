1f43d71bec3bec0c60b594497621b9413360834f
Jurgens du Toit <jrgns@jadeit.co.za>
2023-10-29 13:42:41 +0200

    feat: Prelim / rudimentary cache service

---
+++ lib/ditty/services/cache.rb
@@ -0,0 +1,33 @@
+# frozen_string_literal: true
+
+require 'dalli'
+
+module Ditty
+  module Services
+    class Cache
+      attr_writer :store
+      attr_accessor :ttl
+
+      def initialize(store: nil, ttl: 3_600)
+        @store = store
+        @ttl = ttl
+      end
+
+      def store
+        @store ||= Dalli::Client.new(ENV.fetch('CACHE_URL'), namespace: 'Ditty')
+      end
+
+      def set(key, obj)
+        store.set(key, obj, ttl)
+      end
+
+      def get(key)
+        store.get(key)
+      end
+
+      def delete(key)
+        store.delete(key)
+      end
+    end
+  end
+end
